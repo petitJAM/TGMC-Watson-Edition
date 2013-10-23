fcef80c62e50131c0a22ff6edf5a145616c8350c
Corey <coreyja@gmail.com>
2013-10-23 14:57:53 -0400

    Added code for basic SVC technique

---
+++ svc.py
@@ -0,0 +1,75 @@
+import csv, random
+from sklearn import svm
+from sklearn.preprocessing import StandardScaler
+
+def load_train_csv(input_file='datasets/tgmctrain.csv'):
+	data = []
+	ans = []
+	ids = []
+
+	with open(input_file) as training_data:
+		reader = reader = csv.reader(training_data)
+
+		for row in reader:
+			if random.random() > 0.5:
+				continue
+			ans.append(1.0 if row[-1] == 'true' else 0.0)
+			row = [float(x) for x in row[:-1]]
+			ids.append(row[0])
+			data.append(row[1:])
+			
+		
+
+	return ids, data, ans
+
+def load_eval_csv(input_file='datasets/tgmcevaluation.csv'):
+	data = []
+	ids = []
+
+	with open(input_file) as training_data:
+		reader = reader = csv.reader(training_data)
+
+		for row in reader:
+
+			row = [float(x) for x in row]
+			ids.append(row[0])
+			data.append(row[1:])
+			
+
+	return ids, data
+
+
+if __name__ == '__main__':
+	print 'Loading csv...'
+	ids, data, ans = load_train_csv()
+	print 'CSV loaded. %d rows' % len(data)
+	
+	print 'Start PreProcessing...'
+	pre = StandardScaler()
+	data = pre.fit_transform(data, ans)
+	print 'PreProcessing Done.'
+
+	clf = svm.SVC()
+
+	print 'Fitting SVC...'
+	print clf.fit(data,ans)
+	print 'Training Done'
+
+	print 'Load Eval CSV...'
+	eval_ids, eval_data = load_eval_csv()
+	print 'Eval Loaded'
+
+	print 'Start Predicting'
+	answers = clf.predict(eval_data)
+	print 'Predicting Done'
+	
+	# clear file
+	open("answers.txt", 'w').close()
+
+	# write to answers.txt
+	with open("answers.txt", 'w') as ans:
+		for i in range(len(answers)):
+			# if answers[i] != 0:
+			ans.write(str(eval_ids[i]) + ',%d'%answers[i])
+			ans.write("\n")
+
