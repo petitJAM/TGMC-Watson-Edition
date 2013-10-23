33b56289289b253f1a2f957e8efb6b67cf746ccd
Adam Westerman <westeras@rose-hulman.edu>
2013-10-22 20:59:55 -0400

    adding changes

--- confidence.py
+++ confidence.py
@@ -1,4 +1,4 @@
-import csv
+import csv, math
 
 import pprint
 pp = pprint.PrettyPrinter()
@@ -11,49 +11,65 @@ evaluation_data_filename = dataset_dir + "tgmcevaluation.csv"
 
 output_filename = output_dir + "output.csv"
 
+def determination(x):
+    b = math.fabs(x[0]) > 300 * x[1]
+    b = b and x[1] != x[2]
+    return b
 
 with open(training_data_filename) as training_data:
     reader = csv.reader(training_data)
 
     print "Opened CSV"
     csv_data = []
+    true_rows = {}
+    false_rows = {}
+
     for i in range(0, 318):
         csv_data.append([0, float("-inf"), float("inf")])
+
     for row in reader:
-        true_rows = {}
-        false_rows = {}
 
         row_num = int(row[0]) - 1
-        q_num = int(row[1])
+        q_num = int(float(row[1]))
 
         validity = row[-1]
-        for i, data in enumerate(row[2:-1]):
-            num = float(data)
-            if validity == "true":
+        if validity == "true":
+            if q_num in true_rows:
+                true_rows[q_num].append(row_num)
+            else:
+                true_rows[q_num] = [row_num]
+            for i, data in enumerate(row[2:-1]):
+                num = float(data)
+
                 csv_data[i][0] += num
-                if q_num in true_rows:
-                    true_rows[q_num].append(row_num)
-                else:
-                    true_rows[q_num] = [row_num]
 
-            elif validity == "false":
+                if num > csv_data[i][1]:
+                    csv_data[i][1] = num
+                if num < csv_data[i][2]:
+                    csv_data[i][2] = num
+        else:
+            if q_num in true_rows and q_num in false_rows:
+                if len(false_rows[q_num]) < (len(true_rows[q_num]) * 1):
+                    false_rows[q_num].append(row)
+            elif q_num in true_rows and not q_num in false_rows:
+                false_rows[q_num] = [row] 
+
+    for q_num in false_rows:
+        for row in false_rows[q_num]:
+            for i, data in enumerate(row[2:-1]):
+                num = float(data)
+
                 csv_data[i][0] -= num
-                if q_num in false_rows:
-                    false_rows[q_num].append(row_num)
-                else:
-                    false_rows[q_num] = [row_num]
 
-            if num > csv_data[i][1]:
-                csv_data[i][1] = num
-            if num < csv_data[i][2]:
-                csv_data[i][2] = num
+                if num > csv_data[i][1]:
+                    csv_data[i][1] = num
+                if num < csv_data[i][2]:
+                    csv_data[i][2] = num
 
-        if row_num % 10000 == 0:
-            print row_num
     row_length = len(csv_data)
 
-    print "There are", len(csv_data), "rows with length", row_length, "and", row_length - 3, "features"
-
-    print "Master row: ", csv_data
+    for i, col in enumerate(csv_data):
+        if determination(col):
+            print i
 
     print "Reading Data complete"
--- nearest.py
+++ nearest.py
@@ -1,7 +1,5 @@
 import csv, math
-
-import pprint
-pp = pprint.PrettyPrinter()
+from sklearn.neighbors import KNeighborsClassifier
 
 dataset_dir = "datasets/"
 output_dir = "output/"
@@ -10,32 +8,50 @@ training_data_filename = dataset_dir + "tgmctrain.csv"
 evaluation_data_filename = dataset_dir + "tgmcevaluation.csv"
 
 output_filename = output_dir + "output.csv"
+X = []
+y = []
+eval = []
 
 with open(training_data_filename) as training_data:
-    reader = csv.reader(training_data)
+  reader = csv.reader(training_data)
 
-    print "Opened CSV"
-    csv_data = []
-    true_rows = {}
-    false_rows = {}
+  print "Opened training CSV"
 
-    for i in range(0, 318):
-        csv_data.append([0, float("-inf"), float("inf")])
+  for row in reader:
+    row_data = []
+    for data in row[2:-1]:
+      row_data.append(float(data))
 
-    for row in reader:
+    X.append(row_data)
+      
+    if row[-1] == "true":
+      y.append(1)
+    else:
+      y.append(0)
 
-        row_data = []
-        for data in row:
-            try:
-                row_data.append(float(data))
-            except ValueError:
-                if data == "true":
-                    row_data.append(True)
-                elif data == "false":
-                    row_data.append(False)
-                else:
-                    print "Something weird appeared"
-        csv_data.append(row_data)
+  print "Training data has been read"
+  
+neigh = KNeighborsClassifier(n_neighbors=2, weights='distance')
+neigh.fit(X, y)
 
+print "Data has been fitted"
 
-    print "Reading Data complete"
+with open(evaluation_data_filename) as eval_data:
+  reader = csv.reader(eval_data)
+  
+  print "Opened evaluation CSV"
+  
+  for row in reader:
+    row_data = []
+    for data in row[2:]:
+      row_data.append(float(data))
+      
+    eval.append(row_data)
+    
+  print "Evaluation data has been read"
+  
+ans = neigh.predict(eval)
+
+for i, num in enumerate(ans):
+  if num == 1:
+    print i + 400001
